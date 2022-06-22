486d54bfb4d164642fa039ac0fb72c10241f0a3f
Johnny Silva <johnnysilva.dev@gmail.com>
2022-06-22 14:46:26 -0300

    cuidados com abstracao do angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
+import { PhotoBoardModule } from './shared/components/photo-board/photo-board.module';
 import { PhotoFrameModule } from './shared/components/photo-frame/photo-frame.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,13 +10,16 @@ import { LikeWidgetModule } from './shared/components/like-widget/like-widget.mo
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     LikeWidgetModule,
-    PhotoFrameModule
+    PhotoFrameModule,
+    HttpClientModule,
+    PhotoBoardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/components/photo-board/services/photo-board.service.ts
@@ -0,0 +1,16 @@
+import { Photo } from './../interfaces/photo';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoBoardService {
+
+  constructor(private http: HttpClient) { }
+
+  public getPhotos(): Observable<Photo[]>{
+    return this.http.get<Photo[]>(`http://localhost:3000/photos`);
+  }
+}
